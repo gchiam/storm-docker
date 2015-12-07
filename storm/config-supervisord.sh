7b94297980c5853943733b4186b6dabc1a508f52
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2015-12-07 09:51:59 +0800

    Fix supervisor config path

--- storm/config-supervisord.sh
+++ storm/config-supervisord.sh
@@ -1,5 +1,5 @@
-echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.ini
+echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.ini
+echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.ini
+echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.ini
+echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.ini
